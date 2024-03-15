7cc8c03b75b34882c7e4f8a489f5a4d70f89705b
Tunde <Foltunde@gmail.com>
2024-03-15 15:12:35 +0000

    create the links for src and Arrays

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Employee = require('./lib/Employee');
+
+
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const outputPath = path.join(DIST_DIR, 'index.html');
+const render = require('./src/page.js');
+
+// Arrays for teams and id as place holders
+const teamArr = [];
+const idArr = [];
