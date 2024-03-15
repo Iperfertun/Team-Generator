08785b45899755193e8efd0d4c719fb7252721a3
Tunde <Foltunde@gmail.com>
2024-03-15 14:59:58 +0000

    create the index.js and files dist
    test
    assets
    lib
    employee
    engineer
    manager
    intern all at js
    create the dependecies
    fs
    path
    jest
    inquirer

---
+++ index.js
@@ -0,0 +1,8 @@
+//Dependecies 
+const fs = require('fs');
+const path = require('path');
+const jest = require('jest');
+const inquirer = require('inquirer');
+
+//constructors 
+const manager = require()
