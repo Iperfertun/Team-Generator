0d1742c3642cc01fd8be34186d5c9f397829d4c9
Tunde <Foltunde@gmail.com>
2024-03-15 15:47:32 +0000

    create the manager details or profiles

--- index.js
+++ index.js
@@ -18,3 +18,54 @@ const render = require('./src/page.js');
 // Arrays for teams and id as place holders
 const teamArr = [];
 const idArr = [];
+
+//Start application 
+function initApp() {
+        //prompt id use for manager
+        function addManager() {
+            console.log("Start building your team profile");
+            inquirer.prompt([
+              {
+                type: "input", 
+                name: "managerName",
+                message: "What is manager's name?", 
+                validate: answer => {
+                    if (answer !== "") {
+                        return true; 
+                    }
+                    return "Please enter the team's manager's name.";
+                }
+              },
+              {
+                    type: "input",
+                    name: "managerId", 
+                    message: "what is the manager's ID?",
+                    validate: answer => {
+                        if (answer !=="") {
+                            return true;
+                        }
+                        return "Email can't be untenanted.";
+                    }
+              },
+              {
+                    type: "input", 
+                    name: "managerOfficalNumber",
+                    message: "what is the manager's office Contact? (format: +4424111113)", 
+                    validate: answer => {
+                        const pass =amswer.match (
+                            /^[1-9]\d*$/
+                        );
+                        if (pass) {
+                            return true;
+                        }
+                        return "please enter a correct phone number.";
+                    } 
+              }
+            ]).then(answers => {
+                const manager = new manager(answer.managerName, answer.managerId, answers.managerEmail, answers.managerOfficeNumber);
+                teamArr.push(manager); 
+                idArr.push(answers.managerId);
+                addTeam();
+            });
+        }
+}
